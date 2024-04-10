0e084dbf3bc67ff52ff6a8f62bc61fcde3ba0bba
kashish <kashishagrawal1496@gmail.com>
2024-04-10 23:46:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+var button = document.getElementById("doit");
+
+button.addEventListener("click", function () {
+    let inp = document.getElementById("callfunction");
+    let val = inp.value;
+    val = parseInt(val);
+    console.log(val);
+    if (!isFinite(val)) return;
+
+    playFactorialRecursion(val);
+})
+
+async function playFactorialRecursion(n) {
+    await playNodeAnimation("f(" + n + ")", 2);
+
+    if (n <= 1) return 1;
+
+
+    return n * playFactorialRecursion(n - 1);
+}
+
+// l.
+async function changeFunction(def) {
+
+}
+
+function playNodeAnimation(text, time = 2) {
+    let nodeDiv = document.createElement("div");
+    nodeDiv.innerHTML = text;
+    nodeDiv.classList.add("element");
+    nodeDiv.classList.add("temp");
+    document.querySelector(".stack").appendChild(nodeDiv);
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            nodeDiv.classList.remove("temp");
+            resolve();
+        }, time * 1000)
+    })
+}
+function playNodeDeleteAnimation(nodeDiv){
+
+}
+
+async function myFunction(n) {
+    if (n == 0) return 1;
+    return n * myFunction(n - 1);
+}
+
+// Specify variables to log
+const variablesToLog = ['n'];
+function playRecursion(myFunction,variablesToLog) {
+
+    // Convert the function to a string
+    let myFunctionString = myFunction.toString();
+
+    const functionName = myFunction.name;
+    // Split the function string into lines
+    let lines = myFunctionString.split('\n');
+
+    // Add console.log statements after each line that modifies a specified variable
+
+    let x = variablesToLog.map(variable => {
+        return `
+        try { 
+            l["${variable}"]=${variable};
+        }
+        catch{ }`;
+    })
+    lines = lines.map(line => {
+        return `${line}\n${x};`;
+    });
+    lines.splice (1,0,"await playNodeAnimation(JSON.stringify(l));\n");
+    lines.splice(1, 0, x);
+    lines.splice(1, 0, "let l = {};\n");
+    // Reconstruct the modified function string
+    lines.push(`\n ${functionName}(...arguments)`);
+    myFunctionString = lines.join('\n');
+
+    // Create a new function using Function constructor
+    // console.log(myFunctionString);
+    const AsyncFunction = async function () { }.constructor;
+    const modifiedFunction = new AsyncFunction(myFunctionString);
+
+    // Call the modified function
+    modifiedFunction(6);
+}
+
+playRecursion(myFunction,variablesToLog);
